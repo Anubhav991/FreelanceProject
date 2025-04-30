cecffb2017a8d68b57dfd39b4157ffb7bc2a6de2
Anubhav991 <138884406+Anubhav991@users.noreply.github.com>
2025-05-01 01:00:16 +0530

    moullikpaglu

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/freelance');
 }
-const port = 80;
+const port = process.env.PORT|| 80;
 app.use(session({
     secret: "Abhayislove1234",
     resave: true,
